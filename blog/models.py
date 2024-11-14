1359402abbddb523f4d4763fe31aeb195209c965
student36 <student36@itpark2.local>
2024-11-14 21:24:59 +0500

    qwerty

--- blog/models.py
+++ blog/models.py
@@ -3,8 +3,6 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import revers
 
-#ebat ti lox
-# by aslan
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
